a625617c2aa59ce48551f6d26440ab97e1903936
Baobhan Sith <80159437+Heavrnl@users.noreply.github.com>
2025-03-06 12:17:32 +0800

    Update version.py

--- version.py
+++ version.py
@@ -1,19 +1,19 @@
-VERSION = "1.4.2.1"
+VERSION = "1.4.2.2"
 
 # 版本号说明
 VERSION_INFO = {
     "major": 1,        # 主版本号：重大更新，可能不兼容旧版本
     "feature": 4,      # 功能版本号：添加重要新功能
     "minor": 2,        # 次要版本号：添加小功能或优化
-    "patch": 1,        # 补丁版本号：Bug修复和小改动
+    "patch": 2,        # 补丁版本号：Bug修复和小改动
 } 
 
 
 UPDATE_INFO = """
 
-<blockquote><b>✨ 更新日志 v1.4.2.1</b>
+<blockquote><b>✨ 更新日志 v1.4.2.2</b>
 
-- 修复媒体组所有媒体都超限时，不发送超限提醒的问题
+- 修复设置关键字没有反应的问题
 
 </blockquote>
 """
