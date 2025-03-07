4c0f1691c370b66cd4add6beb469876637518a55
Baobhan Sith <80159437+Heavrnl@users.noreply.github.com>
2025-03-07 16:29:37 +0800

    Update version.py

--- version.py
+++ version.py
@@ -1,18 +1,17 @@
-VERSION = "1.4.5"
+VERSION = "1.4.5.1"
 
 # 版本号说明
 VERSION_INFO = {
     "major": 1,        # 主版本号：重大更新，可能不兼容旧版本
     "feature": 4,      # 功能版本号：添加重要新功能
     "minor": 5,        # 次要版本号：添加小功能或优化
-    "patch": 0,        # 补丁版本号：Bug修复和小改动
+    "patch": 1,        # 补丁版本号：Bug修复和小改动
 } 
 
 
-UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.4.5</b>
+UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.4.5.1</b>
 
-  - 新增 /remove_all_keyword (简写: /rak) 命令
-  - 可以通过关键词删除当前频道绑定的所有规则的关键字，如 "/rak \"hello world\" 'hello world'"
+  - 优化媒体组消息处理逻辑，当所有媒体都被屏蔽时不转发消息
 
 </blockquote>
 """
