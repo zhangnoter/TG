9eaedae7f6d9f22bedfdcf529dde2bcb42490bea
Baobhan Sith <80159437+Heavrnl@users.noreply.github.com>
2025-03-08 08:59:28 +0800

    Update version.py

--- version.py
+++ version.py
@@ -1,17 +1,17 @@
-VERSION = "1.4.6.1"
+VERSION = "1.4.6.2"
 
 # 版本号说明
 VERSION_INFO = {
     "major": 1,        # 主版本号：重大更新，可能不兼容旧版本
     "feature": 4,      # 功能版本号：添加重要新功能
     "minor": 6,        # 次要版本号：添加小功能或优化
-    "patch": 1,        # 补丁版本号：Bug修复和小改动
+    "patch": 2,        # 补丁版本号：Bug修复和小改动
 } 
 
 
-UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.4.6.1</b>
+UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.4.6.2</b>
 
-  - 修复过滤用户名功能不起作用的问题
+  - 增强绑定指令参数解析代码
 
 </blockquote>
 """
