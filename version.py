ac4c0b823300dc06e851ac8f8de680e674c75b1b
Baobhan Sith <80159437+Heavrnl@users.noreply.github.com>
2025-03-20 21:27:20 +0800

    Update version.py

--- version.py
+++ version.py
@@ -1,17 +1,18 @@
-VERSION = "1.6.1.1"
+VERSION = "1.6.1.2"
 
 # 版本号说明
 VERSION_INFO = {
     "major": 1,        # 主版本号：重大更新，可能不兼容旧版本
     "feature": 6,      # 功能版本号：添加重要新功能
     "minor": 1,        # 次要版本号：添加小功能或优化
-    "patch": 1,        # 补丁版本号：Bug修复和小改动
+    "patch": 2,        # 补丁版本号：Bug修复和小改动
 } 
 
 
-UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.6.1.1</b>
+UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.6.1.2</b>
 
-- 修复用户以频道身份在群里发消息时无法获取名字的问题
+- 优化命令参数解析，保持正则表达式原始形式
+- 修复AI处理后再次检查关键字错误的问题
 
 </blockquote>
 """
@@ -25,7 +26,7 @@ WELCOME_TEXT = """
 <blockquote>⭐ <b>给项目点个小小的 Star:</b> <a href='https://github.com/Heavrnl/TelegramForwarder'>TelegramForwarder</a>
 ☕ <b>请我喝杯咖啡:</b> <a href='https://ko-fi.com/0heavrnl'>Ko-fi</a></blockquote>
 
-当前版本: v1.6.1.1
+当前版本: v1.6.1.2
 更新日志: /changelog
 
 感谢您的支持!
