96093f938f185673a01434e7af3395cd27267b8b
Baobhan Sith <80159437+Heavrnl@users.noreply.github.com>
2025-06-22 08:42:11 +0800

    Update version.py

--- version.py
+++ version.py
@@ -1,17 +1,17 @@
-VERSION = "1.7.1.4"
+VERSION = "1.7.2"
 
 # 版本号说明
 VERSION_INFO = {
     "major": 1,        # 主版本号：重大更新，可能不兼容旧版本
     "feature": 7,      # 功能版本号：添加重要新功能
-    "minor": 1,        # 次要版本号：添加小功能或优化
-    "patch": 4,        # 补丁版本号：Bug修复和小改动
+    "minor": 2,        # 次要版本号：添加小功能或优化
+    "patch": 0,        # 补丁版本号：Bug修复和小改动
 } 
 
 
-UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.7.1.4</b>
+UPDATE_INFO = """<blockquote><b>✨ 更新日志 v1.7.2</b>
 
-- 修复删除带有替换规则的转发规则时出现的数据库错误
+- 提高AI总结的健壮性 @iCross https://github.com/Heavrnl/TelegramForwarder/pull/47
 
 </blockquote>
 """
@@ -25,7 +25,7 @@ WELCOME_TEXT = """
 <blockquote>⭐ <b>给项目点个小小的 Star:</b> <a href='https://github.com/Heavrnl/TelegramForwarder'>TelegramForwarder</a>
 ☕ <b>请我喝杯咖啡:</b> <a href='https://ko-fi.com/0heavrnl'>Ko-fi</a></blockquote>
 
-当前版本: v1.7.1.4
+当前版本: v1.7.2
 更新日志: /changelog
 
 感谢您的支持!
